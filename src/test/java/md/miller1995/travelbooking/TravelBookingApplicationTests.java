dc59f3409f16a1ec679ea1c6cd8a1ed810415123
miller1995 <nircaanton95@gmail.com>
2023-05-23 10:41:09 +0300

    This is a new commit for what I originally planned to be amended

---
+++ src/test/java/md/miller1995/travelbooking/TravelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package md.miller1995.travelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
