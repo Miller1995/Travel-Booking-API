b7b036fc7ffef42e3c1ea43b917186f3357184d7
miller1995 <nircaanton95@gmail.com>
2023-06-14 22:03:07 +0300

    - add ControllerAdvice for exception handle reviews and add new exception ReviewNotFoundException;

---
+++ src/main/java/md/miller1995/travelbooking/exceptions/reviews/ReviewExceptionHandlerAdvice.java
@@ -0,0 +1,21 @@
+package md.miller1995.travelbooking.exceptions.reviews;
+
+import md.miller1995.travelbooking.exceptions.users.UserNotFoundException;
+import md.miller1995.travelbooking.utils.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ReviewExceptionHandlerAdvice {
+
+    @ExceptionHandler(ReviewNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleReviewNotFoundException(ReviewNotFoundException exception) {
+        ErrorResponse response = new ErrorResponse(
+                "User wasn't found!",
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/md/miller1995/travelbooking/exceptions/reviews/ReviewNotFoundException.java
@@ -0,0 +1,4 @@
+package md.miller1995.travelbooking.exceptions.reviews;
+
+public class ReviewNotFoundException extends RuntimeException{
+}
